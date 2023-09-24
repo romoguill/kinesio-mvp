b665a9fe75b25ed0c616f20b100a9fe569ed64c5
Rodrigo Moguillansky <romoguill@gmail.com>
2023-09-24 13:26:46 -0300

    Finished basic landing template under construction

--- src/app/about/ContactForm.tsx
+++ src/app/about/ContactForm.tsx
@@ -1,5 +1,25 @@
 function ContactForm() {
-  return <div>ContactForm</div>;
+  return (
+    <div className='basis-1/2 flex flex-col px-10 py-4'>
+      <h3 className='text-neutral-200 text-2xl mb-2 font-semibold'>Contacto</h3>
+      <form className='flex flex-col gap-2 grow'>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          className='px-3 py-2 rounded-md opacity-80'
+        />
+        <textarea
+          name='content'
+          placeholder='Mensaje'
+          className='px-3 py-2 rounded-md opacity-80 h-40'
+        />
+        <button className='rounded-md p-2 text-neutral-200 bg-blue-500/50 mt-2'>
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from './ContactForm';
 
 function Landing() {
   return (
-    <div className='min-h-screen bg-slate-600 '>
+    <div className='min-h-screen bg-slate-800 '>
       <div className='max-w-5xl mx-auto'>
         <div className='flex justify-between items-center px-10 py-8 text-neutral-200'>
           <h3 className='text-2xl'>KINESIO APP</h3>
@@ -15,8 +15,8 @@ function Landing() {
           </Link>
         </div>
 
-        <div className='px-10 flex justify-between'>
-          <div className='basis-1/2 '>
+        <div className='px-10 flex justify-between py-10'>
+          <div>
             <h1 className='text-6xl text-neutral-200'>KINESIO APP</h1>
             <h2 className='text-3xl mt-2 text-neutral-300'>
               App para centralizar tu tratamiento en un solo lugar
@@ -25,8 +25,47 @@ function Landing() {
               SITIO EN CONSTRUCCION
             </h2>
           </div>
-          <ContactForm />
         </div>
+        <div className='flex flex-col md:grid md:grid-cols-3 gap-2 px-10 w-full py-4'>
+          <div className=' bg-slate-700 rounded-md p-4'>
+            <h4 className='text-2xl text-neutral-200 mb-3'>Pacientes</h4>
+            <ul className='space-y-4'>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Plan de entrenamiento
+              </li>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Explicativo de ejercicios
+              </li>
+              <li className='text-neutral-300 text-lg ml-2'>Ver progreso</li>
+            </ul>
+          </div>
+          <div className='bg-slate-700 rounded-md p-4'>
+            <h4 className='text-2xl text-neutral-200 mb-3'>Profesionales</h4>
+            <ul className='space-y-4'>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Seguimiento de pacientes
+              </li>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Uso y creacion de planes personalizados
+              </li>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Historial clinica de pacientes
+              </li>
+            </ul>
+          </div>
+          <div className='bg-slate-700 rounded-md p-4'>
+            <h4 className='text-2xl text-neutral-200 mb-3'>Consultorios</h4>
+            <ul className='space-y-4'>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Gestion de turnos
+              </li>
+              <li className='text-neutral-300 text-lg ml-2'>
+                Gestion de profesionales
+              </li>
+            </ul>
+          </div>
+        </div>
+        <ContactForm />
       </div>
     </div>
   );
