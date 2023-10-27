e84b5672e6a73bab0acb83f2b71dffd49aa79ed6
Rodrigo Moguillansky <romoguill@gmail.com>
2023-10-27 20:18:58 -0300

    Added google sign in

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,6 +5,7 @@ import LoginForm from '@/components/auth/LoginForm';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import AuthProviderButton from '@/components/auth/AuthProviderButton';
 
 async function Login() {
   const supabase = createServerComponentClient({ cookies });
@@ -20,6 +21,15 @@ async function Login() {
   return (
     <AuthContainer>
       <LoginForm />
+      <div className='my-8 relative'>
+        <hr className='h-[2px] border-none bg-neutral-600/20' />
+        <p className='absolute top-[1px] leading-none left-[50%] -translate-x-[50%] -translate-y-[50%] bg-white px-4 text-sm'>
+          or
+        </p>
+      </div>
+      <AuthProviderButton size={20} provider='google'>
+        Signin with Google
+      </AuthProviderButton>
     </AuthContainer>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,9 @@ import UserMenu from './UserMenu';
 function Header() {
   return (
     <header className='sticky h-20 flex items-center justify-between p-8'>
-      <h2>LOGO</h2>
+      <h2 className='tracking-wider font-bold text-2xl text-blue-800'>
+        CareBear
+      </h2>
       <UserMenu />
     </header>
   );
